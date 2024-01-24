4511d6ea9e38629d3935c13571d169c61e60b065
Pujan Gautam <mepujan10@gmail.com>
2024-01-23 23:11:01 -0500

    models registered to admin and migrated

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from abstract.base import BaseModel
+from django.contrib.auth.models import User
 
-# Create your models here.
+JOB_TYPES = (
+    ('full-time', 'Full Time'),
+    ('part-time', 'Part Time'),
+    ('contract', 'Contract'),
+    ('permanent', 'permanent')
+)
+
+JOB_STATUS = (
+    ('active', 'active'),
+    ('inactive', 'inactive')
+)
+
+
+class Category(BaseModel):
+    title = models.CharField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('-updated',)
+        verbose_name_plural = "Categories"
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+
+class Job(BaseModel):
+    title = models.CharField(max_length=200)
+    type = models.CharField(max_length=10, choices=JOB_TYPES)
+    descriptions = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    requirements = models.TextField()
+    application_count = models.IntegerField(default=0)
+    contact_email = models.EmailField()
+    status = models.CharField(max_length=9, choices=JOB_TYPES)
+    featured_job = models.BooleanField(default=False)
+    urgently_hiring = models.BooleanField(default=False)
+    base_salary = models.FloatField(default=0.0)
+    company_logo = models.ImageField(upload_to='company_logo')
+    hiring_person = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-updated',)
+
+    def __str__(self) -> str:
+        return f"{self.title} - job has been created."
