c89ed171b9d14f06152565b2e250dc575598abab
Pujan Gautam <mepujan10@gmail.com>
2024-01-25 16:03:30 -0500

    registration views created, url build and used in navbar

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,13 @@
+from typing import Any
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.views.generic import CreateView
 from django.contrib import messages
-from .forms import LoginForm
+from django.contrib.auth import get_user_model
+from .forms import LoginForm, EmployeeRegistrationForm, JobSeekerRegistrationForm
+
+User = get_user_model()
 
 
 def login_user(request):
@@ -23,8 +29,50 @@ def login_user(request):
     return render(request, 'login.html', {'form': form})
 
 
-def signup(request):
-    return render(request, 'signup.html')
+class SignUpAsEmployee(CreateView):
+    model = User
+    form_class = EmployeeRegistrationForm
+    template_name = 'signup.html'
+    success_url = '/accounts/login'
+    extra_content = {'title': 'Employer Registration'}
+
+    def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        form = self.form_class(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            user = form.save(commit=False)
+            password = form.cleaned_data.get('password1')
+            user.set_password(password)
+            user.save()
+            return redirect('accounts:login')
+        return render(request, 'signup.html', {'form': form})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['extra_content'] = self.extra_content
+        return context
+
+
+class SignUpAsJobSeeker(CreateView):
+    model = User
+    form_class = JobSeekerRegistrationForm
+    template_name = 'signup.html'
+    success_url = '/accounts/login'
+    extra_content = {'title': 'JobSeeker Registration'}
+
+    def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        form = self.form_class(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            user = form.save(commit=False)
+            password = form.cleaned_data.get('password1')
+            user.set_password(password)
+            user.save()
+            return redirect('accounts:login')
+        return render(request, 'signup.html', {'form': form})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['extra_content'] = self.extra_content
+        return context
 
 
 def logout_user(request):
