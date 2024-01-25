0e1c9c41ddf66354d253e407dd31da40e3b541f5
Pujan Gautam <mepujan10@gmail.com>
2024-01-25 13:13:49 -0500

    url path for jobs-list template and views

--- src/urls.py
+++ src/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from .views import homepage
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +27,9 @@ urlpatterns = [
     path('jobs/', include('jobs.urls', namespace='jobs')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
