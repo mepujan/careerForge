cf3ffbf27966c47b90e3d0f9d5f7aeb842cfd2f0
Pujan Gautam <mepujan10@gmail.com>
2024-01-25 16:02:12 -0500

    employer registration and employee registration form created

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,39 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='username', max_length=200)
     password = forms.CharField(
         label='password', max_length=200, widget=forms.PasswordInput())
+
+
+class EmployeeRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'username', 'password1',
+                  'password2', 'gender', 'bio', 'address', 'mobile', 'profile_pic')
+
+    def save(self, commit: bool = True):
+        user = super().save(commit=False)
+        user.role = 'employer'
+        if commit:
+            user.save()
+        return user
+
+
+class JobSeekerRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'username', 'password1',
+                  'password2', 'gender', 'bio', 'address', 'mobile', 'profile_pic')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.role = 'job-seeker'
+        if commit:
+            user.save()
+        return user
