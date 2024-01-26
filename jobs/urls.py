9d31fb0e5404f039d599171e2c3fe1f7e565c9f7
Pujan Gautam <mepujan10@gmail.com>
2024-01-26 16:39:20 -0500

    job detail page added

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import JobListPageView
+from .views import JobListPageView,  job_detail_view
 
 app_name = 'jobs'
 
 urlpatterns = [
-    path('', JobListPageView.as_view(), name='job')
+    path('', JobListPageView.as_view(), name='job'),
+    path('job/<int:id>/', job_detail_view, name='job-detail')
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,11 +1,26 @@
-from django.views.generic import ListView
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView, DetailView
+from django.db.models import Q
+from datetime import date
 from .models import Job
+
 
 # Create your views here.
 
 
 class JobListPageView(ListView):
     model = Job
-    queryset = Job.objects.all()
     template_name = 'jobs-list.html'
     context_object_name = 'jobs'
+    # paginate_by = 3
+
+    def get_queryset(self):
+        qs = Job.objects.filter(Q(status='active'))
+        return qs
+
+
+def job_detail_view(request, id):
+    job = get_object_or_404(Job, id=id)
+    return render(request, 'job-details.html', {'job': job})
