10920f4e394c3659e2bfaf06cf55815d9b67652f
Pujan Gautam <mepujan10@gmail.com>
2024-01-30 16:53:46 -0500

    modeladmin created

--- jobs/admin.py
+++ jobs/admin.py
@@ -5,6 +5,26 @@ admin.site.site_header = "CareerForge"
 admin.site.site_title = "CareerForge"
 admin.site.index_title = "Welcome to CareerForge Admin Page"
 
-admin.site.register(Category)
-admin.site.register(Job)
-admin.site.register(JobApplication)
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'status', 'deadline')
+    search_fields = ('title', 'type', 'status')
+    list_filter = ('title', 'type', 'status',)
+    list_per_page = 20
+
+
+class JobApplicationAdmin(admin.ModelAdmin):
+    list_display = ('applicant', 'status', 'job')
+    list_filter = ('applicant', 'status')
+    list_per_page = 20
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'i_class')
+    list_filter = ('title',)
+    list_per_page = 20
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Job, JobAdmin)
+admin.site.register(JobApplication, JobApplicationAdmin)
