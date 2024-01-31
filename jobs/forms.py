7dc24f523b7eea655e42a623ab4ade85c8c5200c
Pujan Gautam <mepujan10@gmail.com>
2024-01-31 11:14:52 -0500

    modal form created to upload job

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.forms import ModelForm
 from .models import Job
 
@@ -6,3 +7,17 @@ class SearchForm(ModelForm):
     class Meta:
         model = Job
         fields = ('title', 'category', 'location')
+
+
+class PostJobForm(ModelForm):
+    class Meta:
+        model = Job
+        exclude = ('application_count', 'hiring_person')
+
+    def save(self, commit: bool = True) -> Any:
+
+        job = super().save(commit=False)
+        job.hiring_person = self.request.user.id
+        if commit:
+            job.save()
+        return job
